b60768caeb6fce6bbb63ff579c19a06f97aeff40
garv000 <garvsinghal.work@gmail.com>
2026-03-04 21:52:02 +0530

    first commit

--- sections/Hero.js
+++ sections/Hero.js
@@ -184,7 +184,7 @@ const Hero = () => {
         ref={videorRef}
         src="/videos/hero4.mp4"
         className="hero-video absolute inset-0 w-screen h-screen object-cover overflow-hidden"
-        preload="auto"
+        preload="metadata"
         playsInline
         muted
       />
